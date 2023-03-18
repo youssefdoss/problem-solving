813dcf03bca8038f297b489113b1e37df99cf21e
Youssef Doss <youssefdoss97@gmail.com>
2023-03-17 20:39:50 -0600

    Adding best time to buy and sell stock

--- leetcode.js
+++ leetcode.js
@@ -102,3 +102,40 @@ var mergeTwoLists = function(list1, list2) {
 };
 
 ////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Best Time to Buy and Sell Stock
+ *
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the ith day.
+ *
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ *
+ * Return the maximum profit you can achieve from this transaction. If you
+ * cannot achieve any profit, return 0.
+ *
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let left = 0;
+  let right = 1;
+  let max = 0;
+
+  while (right < prices.length) {
+
+      if (prices[left] < prices[right]) {
+          let profit = prices[right] - prices[left];
+          max = Math.max(profit, max);
+      } else {
+          left = right;
+      }
+
+      right++;
+  }
+
+  return max;
+};
+
+////////////////////////////////////////////////////////////////////////////////
