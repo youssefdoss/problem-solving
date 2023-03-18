947cfe0953aac6a8d91d11bb7458f16a4f9a6052
Youssef Doss <youssefdoss97@gmail.com>
2023-03-17 20:03:01 -0600

    Adding Valid Parentheses

--- leetcode.js
+++ leetcode.js
@@ -1,7 +1,11 @@
 /**
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * Two Sum
  *
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
  *
  * You can return the answer in any order.
  *
@@ -22,3 +26,44 @@ var twoSum = function(nums, target) {
       complements[nums[i]] = i
   }
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Valid Parentheses
+ *
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let openStack = [];
+  const correspondingBrackets = {
+    '}': '{',
+    ')': '(',
+    ']': '['
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '{' || s[i] === '(' || s[i] === '[') {
+      openStack.push(s[i]);
+    } else {
+      const lastChar = openStack.pop();
+      if (correspondingBrackets[s[i]] !== lastChar) {
+        return false;
+      }
+    }
+  }
+
+  return openStack.length === 0;
+};
+
+////////////////////////////////////////////////////////////////////////////////
