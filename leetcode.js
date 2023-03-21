af59deaf616cf6ace9ce984da18fb0ee25422243
Youssef Doss <youssefdoss97@gmail.com>
2023-03-21 08:28:19 -0600

    Adding majority element

--- leetcode.js
+++ leetcode.js
@@ -686,3 +686,33 @@ var reverseList = function(head) {
 };
 
 ////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Majority Element
+ *
+ * Given an array nums of size n, return the majority element.
+ *
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You
+ * may assume that the majority element always exists in the array.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  let count = 0;
+  let candidate;
+
+  for (let i = 0; i < nums.length; i++) {
+      if (count === 0) {
+          candidate = nums[i];
+      }
+
+      if (candidate === nums[i]) {
+          count++;
+      } else {
+          count--;
+      }
+  }
+
+  return candidate;
+};
