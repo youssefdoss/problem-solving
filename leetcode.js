51a948712fabe033bbdf0991a22aa73fb23558eb
Youssef Doss <youssefdoss97@gmail.com>
2023-03-21 08:19:27 -0600

    Adding reverse linked list

--- leetcode.js
+++ leetcode.js
@@ -659,3 +659,30 @@ var longestPalindrome = function(s) {
 
   return maxLength;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Reverse Linked List
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (head === null || head.next === null) return head;
+  reversed = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return reversed;
+};
+
+////////////////////////////////////////////////////////////////////////////////
