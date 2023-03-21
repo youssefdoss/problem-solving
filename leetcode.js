8089465f2be80f53b14073de0acce9f9d2163201
Youssef Doss <youssefdoss97@gmail.com>
2023-03-21 08:43:24 -0600

    Adding add binary

--- leetcode.js
+++ leetcode.js
@@ -716,3 +716,31 @@ var majorityElement = function(nums) {
 
   return candidate;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Add Binary
+ *
+ * Given two binary strings a and b, return their sum as a binary string.
+ *
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  let carry = 0;
+  let result = '';
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+      const sum = (i >= 0 ? parseInt(a[i]) : 0) + (j >= 0 ? parseInt(b[j]) : 0) + carry;
+      result = (sum % 2) + result;
+      carry = Math.floor(sum / 2);
+      i--;
+      j--;
+  }
+
+  return result;
+};
