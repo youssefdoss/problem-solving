0075438f4049a22a615c491bbfecc9b4bb85c694
Youssef Doss <youssefdoss97@gmail.com>
2023-03-21 09:44:21 -0600

    Adding contains duplicate

--- leetcode.js
+++ leetcode.js
@@ -855,3 +855,18 @@ var maxDepth = function(root) {
 
   return Math.max(leftDepth, rightDepth) + 1;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Contains Duplicate
+ *
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  return !(new Set(nums).size === nums.length);
+};
