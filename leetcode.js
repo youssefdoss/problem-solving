94e0879ca2c3f901f09d6e8baaa38f8f594c38f3
Youssef Doss <youssefdoss97@gmail.com>
2023-03-21 09:52:54 -0600

    Adding maximum subarray

--- leetcode.js
+++ leetcode.js
@@ -870,3 +870,26 @@ var maxDepth = function(root) {
 var containsDuplicate = function(nums) {
   return !(new Set(nums).size === nums.length);
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Maximum Subarray
+ *
+ * Given an integer array nums, find the subarray with the largest sum, and
+ * return its sum.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let maxSum = nums[0];
+  let currSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+      currSum = Math.max(nums[i], currSum + nums[i]);
+      maxSum = Math.max(maxSum, currSum);
+  }
+
+  return maxSum;
+};
