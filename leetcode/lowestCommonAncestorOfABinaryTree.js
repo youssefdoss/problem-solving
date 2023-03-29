d8d5acb4ce4c026ade267eef7b24527757b405a6
Youssef Doss <youssefdoss97@gmail.com>
2023-03-28 22:49:41 -0400

    Adding lowest common ancestor of a binary tree

--- leetcode/lowestCommonAncestorOfABinaryTree.js
+++ leetcode/lowestCommonAncestorOfABinaryTree.js
@@ -1,18 +1,19 @@
 /**
- * Lowest Common Ancestor of a Binary Search Tree
- *
- * Given a binary search tree (BST), find the lowest common ancestor (LCA) node
- * of two given nodes in the BST.
- *
- * According to the definition of LCA on Wikipedia: “The lowest common ancestor
- * is defined between two nodes p and q as the lowest node in T that has both p
- * and q as descendants (where we allow a node to be a descendant of itself).”
- *
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
  *     this.left = this.right = null;
  * }
+ */
+/**
+ * Lowest Common Ancestor of a Binary Tree
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes
+ * in the tree.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
  *
  * @param {TreeNode} root
  * @param {TreeNode} p
@@ -20,17 +21,18 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-  if (root === null || root === p || root === q) {
+  if (!root || root ===  p || root === q) {
       return root;
   }
 
-  if (root.val > p.val && root.val > q.val) {
-      return lowestCommonAncestor(root.left, p, q);
-  }
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
 
-  if (root.val < p.val && root.val < q.val) {
-      return lowestCommonAncestor(root.right, p, q);
+  if (left && right) {
+      return root;
+  } else if (left) {
+      return left;
+  } else if (right) {
+      return right;
   }
-
-  return root;
 };
