84706d4bd0687e84394d6d5ba787e2848c9f7e86
Youssef Doss <youssefdoss97@gmail.com>
2023-04-26 13:33:01 -0400

    Adding binary tree postorder traversal

--- leetcode/binaryTreePreorderTraversal.js
+++ leetcode/binaryTreePreorderTraversal.js
@@ -17,12 +17,14 @@
  */
 var preorderTraversal = function(root) {
   let result = [];
+
   function traverse(node) {
       if (!node) return;
       result.push(node.val);
       if (node.left) traverse(node.left);
       if (node.right) traverse(node.right);
   }
+
   traverse(root);
   return result;
 };
