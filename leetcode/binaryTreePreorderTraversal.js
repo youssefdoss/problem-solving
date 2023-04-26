ed1950efd179af8af142e4418691674ec4f2b391
Youssef Doss <youssefdoss97@gmail.com>
2023-04-26 13:29:32 -0400

    Adding binary tree preorder traversal

---
+++ leetcode/binaryTreePreorderTraversal.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Binary Tree Preorder Traversal
+ *
+ * Given the root of a binary tree, return the preorder traversal of its nodes'
+ * values.
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  let result = [];
+  function traverse(node) {
+      if (!node) return;
+      result.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+  }
+  traverse(root);
+  return result;
+};
