202875c63b6a5d2bd0a020fc6b67414d7d2e5888
Youssef Doss <youssefdoss97@gmail.com>
2023-04-28 13:57:32 -0400

    Adding isomorphic strings

---
+++ leetcode/isomorphicStrings.js
@@ -0,0 +1,31 @@
+/**
+ * Isomorphic Strings
+ *
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings s and t are isomorphic if the characters in s can be replaced to
+ * get t.
+ *
+ * All occurrences of a character must be replaced with another character while
+ * preserving the order of characters. No two characters may map to the same
+ * character, but a character may map to itself.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  if (s.length !== t.length) return false;
+  let sMap = {};
+  let tMap = {};
+
+  for (let i = 0; i < s.length; i++) {
+      if (sMap[s[i]] !== tMap[t[i]]) {
+          return false;
+      }
+      sMap[s[i]] = i+1;
+      tMap[t[i]] = i+1;
+  }
+
+  return true;
+};
