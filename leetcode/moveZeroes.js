c5cf4de99c26c136e73b6e10b680c4ee0f44f40e
Youssef Doss <youssefdoss97@gmail.com>
2023-05-01 00:35:14 -0400

    Adding move zeroes

---
+++ leetcode/moveZeroes.js
@@ -0,0 +1,20 @@
+/**
+ * Move Zeroes
+ *
+ * Given an integer array nums, move all 0's to the end of it while maintaining
+ * the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let nextNonZeroIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+          [nums[i], nums[nextNonZeroIndex]] = [nums[nextNonZeroIndex], nums[i]];
+          nextNonZeroIndex++;
+      }
+  }
+};
