71965132d470a2a75f991e885b7711b209c4e623
Youssef Doss <youssefdoss97@gmail.com>
2023-05-04 12:32:43 -0400

    Adding fizz buzz

---
+++ leetcode/fizzBuzz.js
@@ -0,0 +1,31 @@
+/**
+ * Fizz Buzz
+ *
+ * Given an integer n, return a string array answer (1-indexed) where:
+ *
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
+ *
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  let answer = [];
+
+  for (let i = 0; i < n; i++) {
+      let k = i + 1;
+      if (k % 3 === 0 && k % 5 === 0) {
+          answer[i] = 'FizzBuzz';
+      } else if (k % 3 === 0) {
+          answer[i] = 'Fizz';
+      } else if (k % 5 === 0) {
+          answer[i] = 'Buzz';
+      } else {
+          answer[i] = k.toString();
+      }
+  }
+
+  return answer;
+};
