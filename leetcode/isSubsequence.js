f938f4aaba058004fb41697a79d4e887f6932ccc
Youssef Doss <youssefdoss97@gmail.com>
2023-05-04 12:21:23 -0400

    Adding is subsequencce

---
+++ leetcode/isSubsequence.js
@@ -0,0 +1,26 @@
+/**
+ * Is Subsequence
+ *
+ * Given two strings s and t, return true if s is a subsequence of t, or false
+ * otherwise.
+ *
+ * A subsequence of a string is a new string that is formed from the original
+ * string by deleting some (can be none) of the characters without disturbing
+ * the relative positions of the remaining characters. (i.e., "ace" is a
+ * subsequence of "abcde" while "aec" is not).
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+  let sIndex = 0;
+
+  for (let i = 0; i < t.length && sIndex < s.length; i++) {
+      if (s[sIndex] === t[i]) {
+          sIndex++;
+      }
+  }
+
+  return sIndex === s.length;
+};
