9cc88c51eb7dec8e4551525a6cf398e4a4aec073
Youssef Doss <youssefdoss97@gmail.com>
2023-05-08 12:03:53 -0400

    Adding number of segments in a string

---
+++ leetcode/numberOfSegmentsInAString.js
@@ -0,0 +1,19 @@
+/**
+ * Number of Segments in a String
+ *
+ * Given a string s, return the number of segments in the string.
+ *
+ * A segment is defined to be a contiguous sequence of non-space characters.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] !== ' ' && (i === 0 || s[i-1] === ' ')) {
+          count++;
+      }
+  }
+  return count;
+};
